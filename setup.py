3d0599c218bf108cc71a360cd608034895834860
itsens <itsens@users.noreply.github.com>
2017-07-07 18:53:30 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='stan',
+    version='0.1',
+    packages=['stan'],
+    url='https://github.com/itsens/stan',
+    license='MIT',
+    author='itsens',
+    author_email='sa@itsens.pro',
+    description='Stat Analyzer'
+)
