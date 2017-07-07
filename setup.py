5278f75a235b39ba3eaa3bfbc48b84a3deb17bb6
itsens <sa@itsens.pro>
2017-07-08 01:12:32 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='stan',
+    version='0.1',
+    packages=['stan', 'stan.parser', 'stan.plotter'],
+    url='https://github.com/itsens/stan',
+    license='MIT',
+    author='itsens',
+    author_email='sa@itsens.pro',
+    description='Stat Analyzer for load testers'
+)
