670b00e345adca1d4c9adfbda7f28fe8d946f5a7
Borodenkov Eugeny <Evgeny.Borodenkov@infotecs.ru>
2017-12-04 18:54:39 +0300

    add unittest

--- examples/sar_example.py
+++ examples/sar_example.py
@@ -5,7 +5,7 @@ import os
 from pprint import pprint
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-TEST_XML = FILE_DIR + '/files/sar_DTDv2.19.xml'
+TEST_XML = 'C:\\Users\\Borodenkov.Evg\\g2g_sg1000\\sar_g2g.xml'
 
 if __name__ == '__main__':
     sar_parser = SarXmlParser()
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     stat = sar_parser.get_stat()
     print(sar_parser.hostname)
     # pprint(stat)
-    pprint(stat.flat())
+    # pprint(stat.flat())
--- tests/test_core_data.py
+++ tests/test_core_data.py
@@ -1,4 +1,6 @@
 import unittest
+from pprint import pprint
+
 from stan.core import StanDict, StanData, StanFlatData
 
 
@@ -12,6 +14,57 @@ class TestStanDict(unittest.TestCase):
         stan_dict_valid = StanDict(metric_1=1, metric_2=2, metric_3=3, metric_4=4)
         stan_dict_result = self.stan_dict_1 + self.stan_dict_2
         self.assertEqual(stan_dict_result, stan_dict_valid)
+
+
+class TestStanData(unittest.TestCase):
+
+    def setUp(self):
+        self.d1 = StanData()
+        self.d1.append(1499763762, StanDict(m1=1, m2=1))
+        self.d1.append(1499763761, StanDict(m1=2, m2=2))
+
+        self.d2 = StanData()
+        self.d2.append(1499763762, StanDict(m4=4, m5=5))
+        self.d2.append(1499763763, StanDict(m4=4, m5=5))
+
+        self.dd = StanData()
+        self.dd.append(1499763762, StanDict(m1=1, m2=1, m4=4, m5=5))
+        self.dd.append(1499763761, StanDict(m1=2, m2=2))
+        self.dd.append(1499763763, StanDict(m4=4, m5=5))
+
+    def test_len_dd(self):
+        dd = self.d1 + self.d2
+        self.assertEqual(dd, self.dd)
+
+    def test_metrics_len(self):
+        dd = self.d1 + self.d2
+        self.assertEqual(len(dd.metrics), 4)
+
+    def test_save_and_load(self):
+        self.d1.save('out.pkl')
+
+        dd1 = StanData.load('out.pkl')
+        pprint(dd1)
+        # print('d1:  ', d1)
+        # self.assertEqual(d1, self.d1)
+        ## TODO: сделать проверку созданного файла
+
+    def test_append_not_StanDict(self):
+        '''
+        исключение если передаем структуру не Standict
+        :return:
+        '''
+        pass
+
+    def test_metrics_intersection(self):
+        '''если в одно время прийдет две одинаковые метки'''
+        pass
+
+    def test_flat(self):
+        pass
+
+    def test_relate(self):
+        pass  # relate
 
 
 if __name__ == '__main__':
